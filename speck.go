babb165a9c9f7398c0c8cb32b86c9f8ddfbdb2f5
Damian Gryski <dgryski@gmail.com>
2018-01-17 22:34:56 -0800

    compiler correctly emites rotate instructions

--- speck.go
+++ speck.go
@@ -7,28 +7,12 @@
 */
 package speck
 
-// hardcoded until https://github.com/golang/go/issues/18254 is fixed
-
-// left circular shift
-func rotl3(x uint64) uint64 {
-	const r = 3
+func rotl(x uint64, r uint) uint64 {
 	return (x << r) | (x >> (64 - r))
 }
 
-func rotr3(x uint64) uint64 {
-	const r = 3
+func rotr(x uint64, r uint) uint64 {
 	return (x << (64 - r)) | (x >> r)
-}
-
-// right circular shift
-func rotr8(x uint64) uint64 {
-	const r = 8
-	return (x << (64 - r)) | (x >> r)
-}
-
-func rotl8(x uint64) uint64 {
-	const r = 8
-	return (x << r) | (x >> (64 - r))
 }
 
 func ExpandKeyAndEncrypt(pt, ct, K []uint64) {
@@ -40,17 +24,17 @@ func ExpandKeyAndEncrypt(pt, ct, K []uint64) {
 	ct1 := pt[1]
 	for i := uint64(0); i < 32; i++ {
 		// encryption
-		ct1 = rotr8(ct1)
+		ct1 = rotr(ct1, 8)
 		ct1 += ct0
 		ct1 ^= A
-		ct0 = rotl3(ct0)
+		ct0 = rotl(ct0, 3)
 		ct0 ^= ct1
 
 		// inline key expansion phase
-		B = rotr8(B)
+		B = rotr(B, 8)
 		B += A
 		B ^= i
-		A = rotl3(A)
+		A = rotl(A, 3)
 		A ^= B
 	}
 
@@ -68,17 +52,17 @@ func ExpandKeyAndDecrypt(pt, ct, K []uint64) {
 	for i := uint64(0); i < 32; i++ {
 		// inline key expansion phase
 		A ^= B
-		A = rotr3(A)
+		A = rotr(A, 3)
 		B ^= i
 		B -= A
-		B = rotl8(B)
+		B = rotl(B, 8)
 
 		// decryption
 		ct0 ^= ct1
-		ct0 = rotr3(ct0)
+		ct0 = rotr(ct0, 3)
 		ct1 ^= A
 		ct1 -= ct0
-		ct1 = rotl8(ct1)
+		ct1 = rotl(ct1, 8)
 	}
 
 	ct[0] = ct0
@@ -91,10 +75,10 @@ func Encrypt(pt, ct, k []uint64) {
 
 	for i := 0; i < 32; i++ {
 		// encryption
-		ct1 = rotr8(ct1)
+		ct1 = rotr(ct1, 8)
 		ct1 += ct0
 		ct1 ^= k[i]
-		ct0 = rotl3(ct0)
+		ct0 = rotl(ct0, 3)
 		ct0 ^= ct1
 	}
 
@@ -109,10 +93,10 @@ func Decrypt(pt, ct, k []uint64) {
 	for i := 31; i >= 0; i-- {
 		// encryption
 		ct0 ^= ct1
-		ct0 = rotr3(ct0)
+		ct0 = rotr(ct0, 3)
 		ct1 ^= k[i]
 		ct1 -= ct0
-		ct1 = rotl8(ct1)
+		ct1 = rotl(ct1, 8)
 	}
 
 	pt[0] = ct0
@@ -126,7 +110,7 @@ func ExpandKey(k, K []uint64) {
 	k[0] = K[0]
 
 	for i := uint64(0); i < 32-1; i++ {
-		l[i+1] = (k[i] + rotr8(l[i])) ^ i
-		k[i+1] = rotl3(k[i]) ^ l[i+1]
+		l[i+1] = (k[i] + rotr(l[i], 8)) ^ i
+		k[i+1] = rotl(k[i], 3) ^ l[i+1]
 	}
 }
